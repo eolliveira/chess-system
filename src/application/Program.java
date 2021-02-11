ae7bc8ca5a12003a23ac2d68b62f7f8dfe46006e
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-02-10 21:31:31 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
+		
+		Position pos = new Position(1, 3);
+		System.out.println(pos);
 	}
 
 }
